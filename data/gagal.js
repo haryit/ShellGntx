fa9182ab2ac55219fea1ee194ef1541c2b10a562
haryit <81818407+haryit@users.noreply.github.com>
2025-03-03 11:32:34 +0700

    Create gagal.js

---
+++ data/gagal.js
@@ -0,0 +1,11 @@
+
+        const lottiePlayer = document.createElement('lottie-player');
+        lottiePlayer.setAttribute('src', 'https://haryonokudadiri.com/check-file/?url=data/alert.json');
+        lottiePlayer.setAttribute('speed', '1');
+        lottiePlayer.setAttribute('style', 'width: 250px; height: 250px;');
+        lottiePlayer.setAttribute('autoplay', '');
+        lottiePlayer.setAttribute('direction', '1');
+        lottiePlayer.setAttribute('mode', 'normal');
+
+        
+        document.getElementById('gagal').appendChild(lottiePlayer);
