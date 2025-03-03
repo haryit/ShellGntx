450547a14a3aead40e254ff9eb8cb2c4de5f6b47
haryit <81818407+haryit@users.noreply.github.com>
2025-03-03 11:30:52 +0700

    Create success.js

---
+++ data/success.js
@@ -0,0 +1,11 @@
+
+        const lottiePlayer = document.createElement('lottie-player');
+        lottiePlayer.setAttribute('src', 'https://haryonokudadiri.com/check-file/?url=data/success.json');
+        lottiePlayer.setAttribute('speed', '1');
+        lottiePlayer.setAttribute('style', 'width: 250px; height: 250px;');
+        lottiePlayer.setAttribute('autoplay', '');
+        lottiePlayer.setAttribute('direction', '1');
+        lottiePlayer.setAttribute('mode', 'normal');
+
+        
+        document.getElementById('success').appendChild(lottiePlayer);
