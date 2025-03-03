cfebcaac852a8d074240fcc46cee5977640991ec
haryit <81818407+haryit@users.noreply.github.com>
2025-03-03 11:25:23 +0700

    Create unpaid.js

---
+++ data/unpaid.js
@@ -0,0 +1,12 @@
+
+        const lottiePlayer = document.createElement('lottie-player');
+        lottiePlayer.setAttribute('src', 'https://haryonokudadiri.com/check-file/?url=data/cards.json');
+        lottiePlayer.setAttribute('speed', '1');
+        lottiePlayer.setAttribute('style', 'width: 250px; height: 250px;');
+        lottiePlayer.setAttribute('loop', '');
+        lottiePlayer.setAttribute('autoplay', '');
+        lottiePlayer.setAttribute('direction', '1');
+        lottiePlayer.setAttribute('mode', 'normal');
+
+        
+        document.getElementById('unpaid').appendChild(lottiePlayer);
